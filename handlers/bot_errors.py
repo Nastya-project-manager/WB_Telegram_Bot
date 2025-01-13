a5bf7b7d00ca2041c417bcb72377572e8366bdfa
nyblun <pchelintseva.daria@gmail.com>
2025-01-13 20:19:10 +0700

    Добавлен шаблон Telegram-бота на базе Aiogram
    - Загружен базовый шаблон бота, работающего на библиотеке Aiogram.
    - Подготовлены основные файлы и структура проекта для дальнейшей разработки.

---
+++ handlers/bot_errors.py
@@ -0,0 +1,10 @@
+from log_config import *
+from aiogram import Router
+from aiogram.types import ErrorEvent
+
+router = Router()
+
+
+@router.error()
+async def error_handler(event: ErrorEvent):
+    logging.critical("Критическая ошибка, вызванная %s", event.exception, exc_info=True)
---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from handlers import (
+    bot_commands,
+    bot_messages,
+    bot_errors,
+    bot_startup)
+
+# Загрузка переменных окружения из файла .env
+load_dotenv()
+
+
+async def main() -> None:
+    bot = Bot(os.getenv('BOT_TOKEN'))
+    dp = Dispatcher()
+    dp.include_routers(
+        bot_commands.router,
+        bot_messages.router,
+        bot_startup.router,
+        bot_errors.router
+    )
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
